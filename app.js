62dac78f2b0bad9929443a5d07f3dbdb621dc42d
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-11-10 17:30:24 -0500

    Fix link to style.css
    Fixing the typo in the link to the stylesheet
    allows css to style the html elements.

---
+++ app.js
@@ -0,0 +1,20 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+console.log("Addition:" + add(5, 9));
+console.log("Subtraction:" + subtract(5, 9));
+console.log("Multiplication:" + multiply(5, 9));
+console.log("Division:" + divide(20, 5));
