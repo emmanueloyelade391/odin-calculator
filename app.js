2a413249c136093beaaa8b2dab1a179debbbf97a
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-11-10 20:20:43 -0500

    Fix script link and implement math operations
    Fixing the typo in the app.js link allowed the
    javascript code to appear on the console. Creating
    functions for the 4 main operations completes a
    portion of the project's logic.

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 function add(x, y) {
+  const first = x;
+  const second = y;
+  const operation = "+";
   return x + y;
 }
 
 function subtract(x, y) {
+  const first = x;
+  const second = y;
+  const operation = "-";
   return x - y;
 }
 
 function multiply(x, y) {
+  const first = x;
+  const second = y;
+  const operation = "*";
   return x * y;
 }
 
 function divide(x, y) {
+  const first = x;
+  const second = y;
+  const operation = "/";
   return x / y;
 }
 
-console.log("Addition:" + add(5, 9));
-console.log("Subtraction:" + subtract(5, 9));
-console.log("Multiplication:" + multiply(5, 9));
-console.log("Division:" + divide(20, 5));
+function operate(x, y, operator) {
+  const first = x;
+  const second = y;
+  const operation = operator;
+  if (operator === "+") {
+    return add(x, y);
+  }
+  if (operator === "-") {
+    return subtract(x, y);
+  }
+  if (operator === "*") {
+    return multiply(x, y);
+  }
+  if (operator === "/") {
+    return divide(x, y);
+  }
+}
+
+console.log(operate(5, 4, "/"));
+
+/*
+console.log(`Addition: ${add(5, 9)}`);
+console.log(`Subtraction: ${subtract(5, 9)}`);
+console.log(`Multiplication: ${multiply(5, 9)}`);
+console.log(`Division:" ${divide(20, 5)}`);
+*/
