b7d399c8af4799e6fa7c8ff4c9da53f98b89bd1d
Emmanuel Oyelade <emmanueloyelade2@gmail.com>
2025-12-01 17:31:02 -0500

    Add every number to the display when clicked
    Making every number appear in the display is a big
    step for making the calculator interactive for the
    user and allowing the user to make calculations.

--- app.js
+++ app.js
@@ -48,7 +48,53 @@ console.log(operate(5, 4, "/"));
 
 const display = document.querySelector("#display");
 let displayString = "";
+
 const oneBtn = document.querySelector("#one-btn");
 oneBtn.addEventListener("click", (event) => {
   display.textContent = display.textContent + "1";
+});
+
+const twoBtn = document.querySelector("#two-btn");
+twoBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "2";
+});
+
+const threeBtn = document.querySelector("#three-btn");
+threeBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "3";
+});
+
+const fourBtn = document.querySelector("#four-btn");
+fourBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "4";
+});
+
+const fiveBtn = document.querySelector("#five-btn");
+fiveBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "5";
+});
+
+const sixBtn = document.querySelector("#six-btn");
+sixBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "6";
+});
+
+const sevenBtn = document.querySelector("#seven-btn");
+sevenBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "7";
+});
+
+const eightBtn = document.querySelector("#eight-btn");
+eightBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "8";
+});
+
+const nineBtn = document.querySelector("#nine-btn");
+nineBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "9";
+});
+
+const zeroBtn = document.querySelector("#zero-btn");
+zeroBtn.addEventListener("click", (event) => {
+  display.textContent = display.textContent + "0";
 });
